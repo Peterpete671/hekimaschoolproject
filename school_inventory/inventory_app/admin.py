9f27521fcc7ad613212be67bb4550374e8da3773
Peter Mwaura <mwaurapeter1202@gmail.com>
2025-12-02 15:34:07 +0300

    Adding admin.py file

--- school_inventory/inventory_app/admin.py
+++ school_inventory/inventory_app/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from django.contrib.auth import get_user_model
+from .models import (TeacherProfile, SchoolClass, Subject, Student,
+                     InventoryCategory, InventoryItem, BorrowLog, Attendance, Grade)
 # Register your models here.
+
+User = get_user_model()
+admin.site.register(User)
+admin.site.register(TeacherProfile)
+admin.site.register(SchoolClass)
+admin.site.register(Subject)
+admin.site.register(Student)
+admin.site.register(InventoryCategory)
+admin.site.register(InventoryItem)
+admin.site.register(BorrowLog)
+admin.site.register(Attendance)
+admin.site.register(Grade)
